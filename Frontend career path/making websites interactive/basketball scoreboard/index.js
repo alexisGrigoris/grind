3ec167d554db93f8ba33d7dcf2485eca09b71ff1
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-01-20 18:34:42 +0200

    basketball scoreboard without reset button

---
+++ Frontend career path/making websites interactive/basketball scoreboard/index.js
@@ -0,0 +1,39 @@
+let home = 0
+let guest = 0
+document.getElementById("hp").textContent = home
+document.getElementById("gp").textContent = guest
+
+function add1() {
+    home += 1;
+    hp.textContent = home;
+
+}
+
+function add2() {
+    home += 2;
+    hp.textContent = home;
+}
+
+function add3() {
+    home += 3;
+    hp.textContent = home;
+}
+
+function gadd1() {
+    guest += 1;
+    gp.textContent = guest;
+}
+
+function gadd2() {
+    guest += 2;
+    gp.textContent = guest;
+}
+
+function gadd3() {
+    guest += 3;
+    gp.textContent = guest;
+}
+
+function reset(){
+
+}
