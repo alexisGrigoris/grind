40e5106107fa19a6df4046769370bedbe3a0d9e6
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-02-01 01:31:50 +0200

    update

--- Frontend career path/chrome extension/index.js
+++ Frontend career path/chrome extension/index.js
@@ -1,22 +1,28 @@
 const inputBtn = document.getElementById("input-btn")
-const inputf = document.getElementById("input-el")
-let myLeads = [1,2,3,4]
+let inputf = document.getElementById("input-el")
+let myLeads = []
 const ulEl = document.getElementById("ulEl")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputf.value)
-    
+    if (inputf.value != '') {
+        myLeads.push(inputf.value)
+        renderLeads()
+        inputf.value =''
+    }   else {
+    }
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-let listItems = ulEl.innerHTML
+function renderLeads(){
 listItems = ""
-// Assign it to an empty string to begin with
 for (let i = 0; i < myLeads.length; i++) {
-    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItems +="<li>" + myLeads[i] + "</li>"
+
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
 }
-// 3. Render the listItems inside the unordered list using ulEl.innerHTML
-
 ulEl.innerHTML = listItems 
-
+}
