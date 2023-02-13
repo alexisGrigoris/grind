0d6a49de3ef89158de4429d2277b22a181cf6546
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-02-13 22:25:36 +0200

    My emojis app finished

--- Frontend career path/emoji/index.js
+++ Frontend career path/emoji/index.js
@@ -1,10 +1,17 @@
 // Push the emoji into the myEmoji's array, and clear the input field
 // However, if the input value is empty, don't do anything
 
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["👨‍💻", "🎮", "⚽", "🥊" ,"🐊"]
 const emojiContainer = document.getElementById("emoji-container")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const emojiInput = document.getElementById("emoji-input")
+const shiftBtn = document.getElementById("shift-btn")
+const popBtn = document.getElementById("pop-btn")
+
 
 function renderEmojis(val) {
+    emojiContainer.innerHTML = ""
     for (let i = 0; i < myEmojis.length; i++) {
         const emoji = document.createElement('span')
         emoji.textContent = myEmojis[i]
@@ -12,13 +19,11 @@ function renderEmojis(val) {
 }}
 
 renderEmojis()
-const pushBtn = document.getElementById("push-btn")
-pushBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input")
+
+pushBtn.addEventListener("click", function push(){
     if (emojiInput.value) {
         
         myEmojis.push(emojiInput.value)
-        emojiContainer.innerHTML = ""
         renderEmojis(emojiInput.value)
         emojiInput.value = ""
         
@@ -28,7 +33,48 @@ pushBtn.addEventListener("click", function(){
     }
 })
 
-/*for(let i=0; i < myEmojis.length; i++) {
+unshiftBtn.addEventListener("click", function unshift(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        renderEmojis(emojiInput.value)
+        emojiInput.value = ""
+        
+        
+    
+    
+    }
+})
+
+shiftBtn.addEventListener("click", function shift(){
+  
+        myEmojis.shift(emojiInput.value)
+        renderEmojis(emojiInput.value)
+   
+        
+        
+    
+    
+    
+})
+
+popBtn.addEventListener("click", function pop(){
+    
+        myEmojis.pop(emojiInput.value)
+        renderEmojis(emojiInput.value)
+      
+        
+        
+    
+    
+    
+})
+
+
+
+/*
+RENDER EMOJIS
+
+for(let i=0; i < myEmojis.length; i++) {
     emojicont.innerHTML += `<span> ${myEmojis[i]} </span>`
 }
 
