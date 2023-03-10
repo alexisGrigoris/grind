a447cebbf359fce35c62665a8dd9007a46eb6659
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-03-10 23:38:36 +0200

    d

--- Frontend career path/cookie consent/index.js
+++ Frontend career path/cookie consent/index.js
@@ -1,5 +1,5 @@
 const cookie = document.getElementById("modal")
-const xBtn = document.getElementById("modal-close-btn")
+const closeButton = document.getElementById("modal-close-btn")
 const form = document.getElementById("consent-form")
 
 setTimeout (function(){
@@ -7,7 +7,7 @@ setTimeout (function(){
 }, 1500)
 
 
-xBtn.addEventListener("click",function(){
+closeButton.addEventListener("click",function(){
     cookie.style.display='none'
 })
 
@@ -15,13 +15,3 @@ form.addEventListener("submit",function(e){
     e.preventDefault()
 
 })
-/*
-Challenge:
-1. Take control of the form element. 
-2. Add an eventListener to the form to listen for a
-   "submit" event.
-3. When a user clicks "accept", prevent the default
-   behaviour that triggers the refresh.
-4. Log out "form submitted".
-*/  
-
