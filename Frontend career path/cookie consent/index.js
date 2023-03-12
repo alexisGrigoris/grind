fe47d1f5f279e1dfa07099f0c9f64f51fdbd4db1
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-03-12 21:46:01 +0200

    js
    new FormData, FormData.get()

--- Frontend career path/cookie consent/index.js
+++ Frontend career path/cookie consent/index.js
@@ -15,8 +15,11 @@ closeButton.addEventListener("click",function(){
 
 form.addEventListener("submit",function(e){
     e.preventDefault()
-    text.innerHTML = `
+      
+    const  formInfo = new FormData(form)
+    const formName = formInfo.get('fullname')
 
+    text.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
@@ -26,7 +29,7 @@ form.addEventListener("submit",function(e){
     }, 1500)
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${formName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/bob.gif">
