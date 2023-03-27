0f5ea1b211a8dbdd96c520d03101b34c8d6f023d
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-03-27 13:19:29 +0300

    u

--- Frontend career path/Meme picker/index.js
+++ Frontend career path/Meme picker/index.js
@@ -1,17 +1,31 @@
 import { catsData } from './data.js'
-const radiosField = document.getElementById('emotion-radios')
 
+const emotionRadios = document.getElementById('emotion-radios')
+const test = document.getElementById('test')
+
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+/*
+Challenge:
+1. highlightCheckedOption should take control 
+   of the selected radio input and add the CSS
+   class of "highlight" to its classlist
+*/
+    console.log(e.target.id)
+}
 function getEmotionsArray(cats){
     const catsEmotes = []
-
-    for (let cat of cats){
-        for(let emote of cat.emotionTags){
-           if (!catsEmotes.includes(emote)){
-            catsEmotes.push(emote)
+        for (let cat of cats){
+            for(let emote of cat.emotionTags){
+            if (!catsEmotes.includes(emote)){
+                catsEmotes.push(emote)
+                }
             }
+            
         }
-        
-    }
 
     return catsEmotes
 }
@@ -38,7 +52,7 @@ function renderEmotionsRadios(cats){
         </div>
         `
     }
-        radiosField.innerHTML = radioItems
+        emotionRadios.innerHTML = radioItems
     }
 
     renderEmotionsRadios(catsData)
