bdf4ec024de0b2af9b319aa053db32be205190e1
alexisGrigoris <alexisgrigoris@gmail.com>
2023-03-30 05:03:16 +0300

    Meme picker finished

--- Frontend career path/Meme picker/index.js
+++ Frontend career path/Meme picker/index.js
@@ -1,91 +1,110 @@
 import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
-const test = document.getElementById('test')
-const getImgBtn = document.querySelector('.get-image-btn');
-const checkbox = document.getElementById('gifs-only-option')
-
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const closeModalBtn = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
-getImgBtn.addEventListener('click',getMatchingCatsArray) 
+
+closeModalBtn.addEventListener('click',closeModal)
+
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
-
     const radios = document.getElementsByClassName('radio')
-    for (let radio of radios) {
-        radio.classList.remove('highlight');
+    for (let radio of radios){
+        radio.classList.remove('highlight')
     }
-
-document.getElementById(e.target.id).parentElement.classList.add('highlight')
-
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
-function getMatchingCatsArray() {
+function closeModal(){
+    memeModal.style.display = 'none'
+}
 
-    if(document.querySelector('input[type="radio"]:checked')){    
-        const selectedEmotion = document.querySelector('input[type=radio]:checked').value;
-        const gifOnly = checkbox.checked
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
         
         const matchingCatsArray = catsData.filter(function(cat){
-            if (gifOnly) { // if gifOnly alone equals to if gifOnly === true
-                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
-            }else{
-                return cat.emotionTags.includes(selectedEmotion)
-            }
-            /*
-Challenge:
-1. Change the .filter() method's function so it returns an 
-   array that only has GIFs if the 'GIFs only' option is 
-   checked. If the 'GIFs only' option is not checked, it
-   should return an array of all matches as it does now.
-*/ 
-
-
-
-        })
-        
-
-    }
-
-}
-
-function getEmotionsArray(cats){
-    const catsEmotes = []
-        for (let cat of cats){
-            for(let emote of cat.emotionTags){
-            if (!catsEmotes.includes(emote)){
-                catsEmotes.push(emote)
-                }
-            }
             
-        }
-
-    return catsEmotes
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingCatsArray 
+    }   
 }
 
-console.log(getEmotionsArray(catsData)) 
+function getSingleCatObject(){
+    
+    const catsArray = getMatchingCatsArray()
+    
+    if (catsArray.length === 1){
+        return catsArray[0]
+    }
+    else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+    
+}
+
+function renderCat(){
+    
+    const catObject = getSingleCatObject()
+    
+    memeModalInner.innerHTML = `
+    <img 
+    class="cat-img" 
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >
+    `
+    memeModal.style.display = 'flex'
+}
+ 
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
 
 function renderEmotionsRadios(cats){
-    const emotions = getEmotionsArray(cats)
+        
     let radioItems = ``
-    for(let emote of emotions) {
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
         radioItems += `
         <div class="radio">
-
-            <label for = "${emote}"> ${emote} </label>
-
-            <input 
-            type = "radio"
-            value = "${emote}"
-            id = "${emote}"
-            name = "emotions">
-            
-           
-
-        </div>
-        `
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
     }
-        emotionRadios.innerHTML = radioItems
-    }
+    emotionRadios.innerHTML = radioItems
+}
 
-    renderEmotionsRadios(catsData)
+renderEmotionsRadios(catsData)
+
+
+
+
