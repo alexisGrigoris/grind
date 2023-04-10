1343a6e5f806bea0d0e653297ccd32bc1feb21db
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-04-10 06:23:55 +0300

    u

--- Frontend career path/twitter clone/index.js
+++ Frontend career path/twitter clone/index.js
@@ -2,19 +2,33 @@ import { tweetsData } from './data.js'
 
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
-const test = document.getElementById('feed')
+const feed = document.getElementById('feed')
 
 tweetBtn.addEventListener('click', textareaValue)
 
 function textareaValue() {
-    test.textContent = tweetInput.value
+    feed.textContent = tweetInput.value
 }
 
 function getFeedHtml(){
 
     let feedHtml = ``
-    for (let tweet of tweetsData){
-        feedHtml += `<div class="tweet">
+    tweetsData.forEach(function(tweet){
+
+    /*
+Challenge:
+1. Inside each span that has a class of "tweet-detail",
+   add an <i> tag.
+2. Give each <i> tag the classes it needs to render the
+   correct icons next to the numbers.
+   The classes you will need are:
+    fa-regular, 
+    fa-solid, 
+    fa-comment-dots, 
+    fa-heart, 
+    fa-retweet
+*/
+    feedHtml += `<div class="tweet">
         <div class="tweet-inner">
             <img src="${tweet.profilePic}" class="profile-pic">
             <div>
@@ -22,58 +36,33 @@ function getFeedHtml(){
                 <p class="tweet-text">${tweet.tweetText}</p>
                 <div class="tweet-details">
                     <span class="tweet-detail">
-                        ${tweet.replies.length}
+                        <i class="fa-regular fa-comment-dots"> 
+                        </i>
+                            ${tweet.replies.length}
                     </span>
                     <span class="tweet-detail">
-                        ${tweet.likes}
+                        <i class="fa-solid fa-heart">
+                        </i> 
+                            ${tweet.likes}
                     </span>
                     <span class="tweet-detail">
-                        ${tweet.retweets}
+                        <i class="fa-solid fa-retweet">
+                        </i>
+ 
+                            ${tweet.retweets}
                     </span>
                 </div>   
             </div>            
         </div>
     </div>`
-    test.innerHTML = feedHtml
-    }
-    /*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-   create HTML string for each tweet using the 
-   boilerplate below. Replace UPPERCASE text
-   with data from the tweets. 
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml.
-4. Call getFeedHtml to check it's working.
-*/  
-
-
-
-/*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
-*/
-
+   
+})
+return feedHtml
 
 }
 
-getFeedHtml()
+function render(){
+    feed.innerHTML = getFeedHtml()
+}
+
+render();
