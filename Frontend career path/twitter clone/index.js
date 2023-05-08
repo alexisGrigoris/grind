da4de8875f2e97b3a3b57c78a5eb1396ed60598b
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-05-08 17:40:18 +0300

    Update index.js

--- Frontend career path/twitter clone/index.js
+++ Frontend career path/twitter clone/index.js
@@ -1,5 +1,4 @@
-import { tweetsData } from './data.js'
-
+ import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 const feed = document.getElementById('feed')
@@ -14,8 +13,18 @@ document.addEventListener('click', function(e){
     if (e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
     }
+    else if (e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
 })
 
+
+/*
+Challenge:
+1. Make this eventListener call "handleRetweetClick" 
+   when the retweet icon is clicked, passing in the
+   uuid from that tweet.  
+*/
 function handleLikeClick(tweetId){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
@@ -30,6 +39,27 @@ function handleLikeClick(tweetId){
     render()
 }
 
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    } else{
+        targetTweetObj.retweets++
+    }
+       targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+    /*
+    Challenge:
+    2. Find the retweeted tweet's object in tweetsData 
+       and save it to a const.
+    3. Increment or decrement the retweet c ount of the 
+       tweet and flip its isRetweeted boolean.
+    4. Call the render function.  
+    */   
+    }
 
 
 function getFeedHtml(){
