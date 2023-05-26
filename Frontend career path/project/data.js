f7d8899df5aad7c29525c5007bdb35e27fa49c22
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-05-26 15:30:13 +0300

    u

--- Frontend career path/project/data.js
+++ Frontend career path/project/data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni, mushrom, mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        available: true
     },
     {
         name: "Hamburger",
         ingredients: ["beef, cheese, lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        available: true
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        available: true
     }
 ]
