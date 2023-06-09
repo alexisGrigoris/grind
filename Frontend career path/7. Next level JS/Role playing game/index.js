bb01090a04185b9f7ac773942520da70c65ca296
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-06-09 18:10:46 +0300

    u

--- Frontend career path/7. Next level JS/Role playing game/index.js
+++ Frontend career path/7. Next level JS/Role playing game/index.js
@@ -1,53 +1,45 @@
-/*
-CHALLENGE
-1. Convert our consts into two objects called 
-"monster" and "hero".
-2. Update the renderCharacter() function so that it accepts 
-a single object "data" as its parameter instead of five string/numbers, 
-reducing the number of arguments to pass in from five to one.
-3. Update the template now each variable is coming from "data".
-4. Update the function call.
-*/
-const hero = 
-{ 
+const hero = {
   elementId: "hero", 
-  name : "Wizard",
-  avatar : "images/wizard.png",
-  health : 60,
-  diceRoll : 6,
+  name: "Wizard",
+  avatar: "images/wizard.png",
+  health: 60,
+  diceRoll: [3, 1, 4],
   diceCount: 3
-
 }
 
-const monster = 
-{     
-  elementId: "monster", 
-  name : "Orc",
-  avatar : "images/orc.png",
-  health : 10,
-  diceRoll : 4,
+const monster = {
+  elementId: "monster",
+  name: "Orc",
+  avatar: "images/orc.png",
+  health: 10,
+  diceRoll: [2],
   diceCount: 1
-
 }
 
+function renderCharacter(data) {
+  const {elementId, name, avatar, health, diceRoll, diceCount } = data;
+  let diceHtml = '';
 
-function renderCharacter(data){
-  const {elementId, name, avatar, health, diceRoll, diceCount} = data
+// CHALLENGE
+// Update this for loop so it uses a value from the 
+// new diceRoll array to render out the dice so the 
+// wizard's dice have values of 3, 1 and 4, and the
+// orc's single dice has a value of 2.
+
+   
+for (let i = 0; i < diceCount; i++) {
+  diceHtml += `<div class="dice">6</div>`
+}
   
-  let diceHtml = ''
-
-  for ( let i = 0;  i < diceCount; i++) {
-    diceHtml += `<div class="dice">6</div>`
-  }
-
-
   document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health}</b></p>
-        <div class="dice-container">${diceHtml} </div>
-    </div>`;   
+  `<div class="character-card">
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}" />
+      <div class="health">health: <b>${health}</b></div>
+      <div class="dice-container">
+          ${diceHtml}
+      </div>
+  </div>`; 
 }
 
 renderCharacter(hero);
