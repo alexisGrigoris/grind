03ca0cfa774b488ca912c9c6cb80e368c62eeef9
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-07-17 18:32:22 +0300

    Update index.js

--- Frontend career path/7. Next level JS/Role playing game/index.js
+++ Frontend career path/7. Next level JS/Role playing game/index.js
@@ -6,6 +6,33 @@ function getDiceRollArray(diceCount) {
  
 }
 
+/* const hotel2 = {
+	name: 'Leopard Mansion',
+	rooms: 96,
+	stars: '⭐⭐⭐',
+	costPerNightAdult: 120,
+	costPerNightChild: 180,
+}
+
+
+function NationalParkHotels(data){
+    this.name = data.name
+    this.rooms = data.rooms
+    this.stars = data.stars
+    this.costPerNightAdult = data.costPerNightAdult
+    this.costPerNightChild = data.costPerNightChild
+	
+	this.summariseHotel = function(){
+		const totalPrice = this.costPerNightAdult * 2 + this.costPerNightChild * 2
+		console.log(`A one night stay at the ${this.name} for two adults and 
+		two children costs a total of ${totalPrice}`)
+	}
+}
+
+const safariView = new NationalParkHotels(hotel1)
+const leopardMansion = new NationalParkHotels(hotel2)
+safariView.summariseHotel()
+leopardMansion.summariseHotel() */
 
 
 function getDiceHtml(diceCount){
