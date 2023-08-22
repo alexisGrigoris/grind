1fbfdd657aa7fc3b3774e6f24a74f64ca2a82582
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-08-22 18:33:10 +0300

    u

--- Frontend career path/7. Next level JS/Role playing game/character.js
+++ Frontend career path/7. Next level JS/Role playing game/character.js
@@ -1,7 +1,23 @@
-import {getDiceRollArray} from './utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
+
+
+/*
+CHALLENGE
+1. In the Character constructor, create a new 
+property called "diceArray".
+2. Set diceArray equal to whatever is returned
+by the getDicePlaceholderHtml function (think 
+what two things you need to do to make this work!!).
+3. Instead of rendering diceHtml in the getCharacterHtml 
+method, render diceArray.
+4. Delete any unnecessary code.
+*/
+
 
 function Character(data) {
     Object.assign(this, data)
+
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
     
     this.getDiceHtml = function(diceCount) {
         return getDiceRollArray(diceCount).map(function(num){ 
@@ -10,7 +26,7 @@ function Character(data) {
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
+        const { elementId, name, avatar, health, diceCount, diceArray } = this;      
         let diceHtml = this.getDiceHtml(diceCount);
         
            return `
@@ -19,7 +35,7 @@ function Character(data) {
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${diceArray}
                 </div>
             </div>`;
     }  
