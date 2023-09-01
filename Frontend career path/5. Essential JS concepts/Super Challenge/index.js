2fadc053de3ae17176068d4707c2db3682e33847
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-09-01 11:06:37 +0300

    u

--- Frontend career path/5. Essential JS concepts/Super Challenge/index.js
+++ Frontend career path/5. Essential JS concepts/Super Challenge/index.js
@@ -1,12 +1,32 @@
-import propertyForSaleArr from "propertyForSaleArr.js"
-import placeholderPropertyObj from "placeholderPropertyObj.js"
+import propertyForSaleArr from './properties/propertyForSaleArr.js'
+import placeholderPropertyObj from './properties/placeholderPropertyObj.js'
+
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
+    return propertyArr.map(property => {
+        const {
+            propertyLocation,
+            priceGBP,
+            roomsM2,
+            comment,
+            image
+        } = property
+
+        const totalM2 = roomsM2.reduce((total, curr) => total + curr
+        )
+        return `
+            <section class="card">
+                <img src="./images/${image}">
+                <div class="card-right">
+                    <h2>${propertyLocation}</h2>
+                    <h3>${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${totalM2} m&sup2;</h3>
+                </div>
+            </section>` 
+    }).join('')
 
 
-
-function getPropertyHtml() {
-
-    
-    /*
+/*
 SUPER CHALLENGE 💪
 
 Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
@@ -38,4 +58,3 @@ This is the HTML template 👇. Replace everything in UPPERCASE with property da
 
 /***** Modify 👇 by adding an argument to the function call ONLY. *****/
 document.getElementById('container').innerHTML = getPropertyHtml()
-
