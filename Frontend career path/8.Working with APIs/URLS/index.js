a51fc34570748468a7201b09f16fd5e9268030cb
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-09-19 18:27:54 +0300

    u

--- Frontend career path/8.Working with APIs/URLS/index.js
+++ Frontend career path/8.Working with APIs/URLS/index.js
@@ -22,5 +22,50 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
                     <hr>
             `
         }
-        document.getElementById('cont').innerHTML = html
+        document.getElementById('blog-list').innerHTML = html
     })
+
+
+
+/**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+       /**
+     * Challenge: Send this off to the server!
+     * 
+     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+     * 2. Endpoint: /posts
+     * 3. method: ???
+     * 4. Request body: ??? (Remember to turn it into JSON)
+     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+     */
+    
+       const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
+
+
