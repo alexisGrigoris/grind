6ef9ed2dcff0e3343361c5878a9634339f95e086
Alexis Grigoris <alexisgrigoris@gmail.com>
2023-09-21 12:44:58 +0300

    u

--- Frontend career path/8.Working with APIs/URLS/index.js
+++ Frontend career path/8.Working with APIs/URLS/index.js
@@ -1,41 +1,23 @@
-/**
- Challenge:
+let postsArray = []
 
- With the 5 blog post objects, display the `title` and `body`
-properties of the first 5 posts on the browser page.
- 
- Hints: 
- * Create a `div` in the HTML file to store these items
- * Loop over the items creating a string of HTML elements you 
-   can then put into the div with `innerHTML`
- */
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
 
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0,5)
-        let html = ""
-        for(let post of postsArr){
-            html += `<h2>${post.title} </h2>
-                    <p>${post.body} </p>
-                    <hr>
-            `
-        }
-        document.getElementById('blog-list').innerHTML = html
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
-
-
-
-/**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
-
-*/
 
 document.getElementById("new-post").addEventListener("submit", function(e) {
     e.preventDefault()
@@ -45,17 +27,8 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
         title: postTitle,
         body: postBody
     }
-       /**
-     * Challenge: Send this off to the server!
-     * 
-     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
-     * 2. Endpoint: /posts
-     * 3. method: ???
-     * 4. Request body: ??? (Remember to turn it into JSON)
-     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
-     */
     
-       const options = {
+    const options = {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -65,7 +38,8 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
     
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+        })
 })
-
-
